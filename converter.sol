f76a8ecdf5ca5dfd7e63de365e4671aacdbc8c85
dingo <106747559+dingo2077@users.noreply.github.com>
2023-04-04 13:34:07 +0300

    Create converter.sol
    descrip

---
+++ converter.sol
@@ -0,0 +1,16 @@
+//SPDX-License: MIT
+pragma =0.8.19;
+
+
+contract Converter {
+
+
+//=================FROM BYTES32=================
+
+//to uint or address;
+  bytes32 hash;
+  return address(uint160(uint256(hash)));
+
+
+
+}
